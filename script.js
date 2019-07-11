f3130cd4cea2ad09f1a688f01adf646be387d393
Tetach Rattanavikran <47658385+theethaj@users.noreply.github.com>
2019-07-11 13:19:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 const baseURL = 'https://exceed.superposition.pknn.dev'
+let state = {}
+function get_data(){
+    fetch (baseURL + '/data/newton')
+      .then((res) => res.text())
+      .then((data) => state = data)
+      .catch((err) => console.log(err));
+      console.log(data)
+}
+function post(door,buzzer,light){
+    fetch(baseURL + '/data/newton',{
+        method: 'POST',
+        body: JSON.stringify({
+        'data': {
+            'door': door,
+            'buzzer':buzzer,
+            'light':light,
+            }
+        }),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then((res) => res.json)
+    .then((data)=>console.log(data))
+    .catch((err)=>console.log(err));
+}
 function toggle_door(){
-    fe
+    const status = get_data()
+    
 }
