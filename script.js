1a46c81fe5cbb3de0bdb06b8e6e40450648240e0
frostdragonite <52771664+frostdragonite@users.noreply.github.com>
2019-07-11 15:08:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const baseURL = 'https://exceed.superposition.pknn.dev'
 let state = {'door':'close','buzzer':'off','light':'off'}
+let refresh = true
+let b = true
+let loc = window.location.pathname.split("/");
+let temp = '';
+console.log(loc)
+for(let i = 0; i < loc.length - 1; i++) {
+    temp = temp + loc[i] + "/"
+}
+console.log(temp)
+
 function get_data(){
+    refresh = false
     fetch (baseURL + '/data/newton')
       .then((res) => res.text())
       .then((data) => state = JSON.parse(data))
@@ -8,6 +19,7 @@ function get_data(){
       
 }
 function toggle_door(){
+    refresh = false
     get_data()
     switch (state['door']){
         case 'open':
@@ -19,16 +31,22 @@ function toggle_door(){
     }
     post()
     get_data()
+    refresh = true
+    refreshpost()
 }
 function alarm_off(){
+    refresh = false
     get_data()
     state['buzzer'] = 'off'
     state['light'] = 'off'
     post()
     get_data()
+    refresh = true
+    refreshpost()
 }
 
 function post(){
+    refresh = false
     fetch(baseURL + '/data/newton',{
         method: 'POST',
         body: JSON.stringify({
@@ -54,9 +72,11 @@ function post(){
     }
     switch (state['buzzer']){
         case 'on':
+            b = true
             blink();
             break;
         case 'off':
+            b = false
             document.getElementById('alarm').src ='alarm_off(1).png';
             break;
     }
@@ -65,21 +85,32 @@ function blink(){
     x = 0.5; 
 
     switch (document.getElementById('alarm').src){
-        case 'alarm_on(1).png':
+        case "file://" + temp + "alarm_on(1).png":
             document.getElementById('alarm').src ='alarm_off(1).png';
             break;
-        case 'alarm_off(1).png':
+        case "file://" + temp + "alarm_off(1).png":
             document.getElementById('alarm').src ='alarm_on(1).png';
             break;
 
     }
 
-    setTimeout(refreshData, x*1000);
+    if (b == true){
+        setTimeout(blink, x*1000);
+    }
+    else {
+        document.getElementById('alarm').src ='alarm_off(1).png';
+    }
 }
 function refreshpost(){
     x = 0.5;
     get_data()
     post()
+    if (refresh == true){
+        setTimeout(refreshpost, x*1000);
+    }
 
-
+    
+}
+function stop_refresh(){
+    refresh = false;
 }
